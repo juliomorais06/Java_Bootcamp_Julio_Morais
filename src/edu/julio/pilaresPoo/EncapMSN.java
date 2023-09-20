5330c16db88af64ce2ba7eb60e63747007c5aaaa
juliomorais06 <142819851+juliomorais06@users.noreply.github.com>
2023-09-20 12:05:50 -0300

    commit encapsulamento Poo

---
+++ src/edu/julio/pilaresPoo/EncapMSN.java
@@ -0,0 +1,18 @@
+package pilaresPoo;
+
+public class EncapMSN {
+    public void enviarMensagem() {
+        validarConectadoInternet();
+        System.out.println("Enviando mensagem");
+        salvarHistoricoMensagem();
+    }
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem");
+    }
+    private void validarConectadoInternet() {
+        System.out.println("Validando se está conectado à internet");
+    }
+    private void salvarHistoricoMensagem() {
+        System.out.println("Salvando o histórico da mensagem");
+    }
+}
---
+++ src/edu/julio/pilaresPoo/Encapsulamento.java
@@ -0,0 +1,10 @@
+package pilaresPoo;
+
+public class Encapsulamento {
+    public static void main(String[] args) {
+        EncapMSN msn = new EncapMSN();
+
+        msn.enviarMensagem();
+        msn.receberMensagem();
+    }
+}
